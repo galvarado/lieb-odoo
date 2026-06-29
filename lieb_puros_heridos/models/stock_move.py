583428fb57434861624abe3da6b6590e9b27c321
guillermo <guillermoalvarado89@gmail.com>
2026-06-29 17:38:31 -0600

    fix: también rellenar product_uom_id faltante en move lines creadas internamente

--- lieb_puros_heridos/models/stock_move.py
+++ lieb_puros_heridos/models/stock_move.py
@@ -18,15 +18,14 @@ class StockMoveLine(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if vals.get('company_id'):
-                continue
+            move = None
             if vals.get('move_id'):
                 move = self.env['stock.move'].browse(vals['move_id'])
-                company = move.company_id or (move.picking_id.company_id if move.picking_id else None)
-            elif vals.get('picking_id'):
-                picking = self.env['stock.picking'].browse(vals['picking_id'])
-                company = picking.company_id
-            else:
-                company = None
-            vals['company_id'] = (company.id if company else None) or self.env.company.id
+            if not vals.get('company_id'):
+                company = (move.company_id if move else None) or \
+                    (self.env['stock.picking'].browse(vals['picking_id']).company_id
+                     if vals.get('picking_id') else None)
+                vals['company_id'] = (company.id if company else None) or self.env.company.id
+            if not vals.get('product_uom_id') and move and move.product_uom:
+                vals['product_uom_id'] = move.product_uom.id
         return super().create(vals_list)
