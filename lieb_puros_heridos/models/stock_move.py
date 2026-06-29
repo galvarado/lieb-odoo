514c3f54beaeadd342d2c3d5d990cf7439a54950
guillermo <guillermoalvarado89@gmail.com>
2026-06-29 17:35:41 -0600

    fix: override StockMoveLine.create para garantizar company_id siempre presente

--- lieb_puros_heridos/models/stock_move.py
+++ lieb_puros_heridos/models/stock_move.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
@@ -10,3 +10,23 @@ class StockMove(models.Model):
         ondelete='restrict',
     )
     nota_rechazo = fields.Text(string='Nota de Rechazo')
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('company_id'):
+                continue
+            if vals.get('move_id'):
+                move = self.env['stock.move'].browse(vals['move_id'])
+                company = move.company_id or (move.picking_id.company_id if move.picking_id else None)
+            elif vals.get('picking_id'):
+                picking = self.env['stock.picking'].browse(vals['picking_id'])
+                company = picking.company_id
+            else:
+                company = None
+            vals['company_id'] = (company.id if company else None) or self.env.company.id
+        return super().create(vals_list)
